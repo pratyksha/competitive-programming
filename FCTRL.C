daf90b6b183556ff33a1bf3ff60321be266479b5
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-08-09 00:16:39 +0530

    Solution to Codechef problem FCTRL
    Link to the problem - https://www.codechef.com/problems/FCTRL

---
+++ FCTRL.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+	int t;
+	scanf("%d\n",&t);
+	while((t--)>0)
+	{
+	 int p=0,ct=0,i=5;
+	 scanf("%d\n",&p);
+	 if(p<5)
+	 {
+	     printf("0\n");
+	     continue;
+	 }
+	 else{
+	 for ( i=5; p/i>=1; i *= 5)
+          ct += p/i;
+	 printf("%d\n",ct);
+	}
+	}
+	return 0;
+}
