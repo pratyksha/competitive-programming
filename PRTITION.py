154991b58d0feb585756cd780bb2908e3bdb048b
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-08-29 21:03:25 +0530

    Solution to Codechef problem PRTITION
    Link to the problem - https://www.codechef.com/problems/PRTITION

---
+++ PRTITION.py
@@ -0,0 +1,20 @@
+def partition_it(n,sums,x):
+    a = [0 for i in range(n)]
+    a[x-1] = 2
+    for j in range(n,0,-1):
+        if a[j-1] != 2:
+            if j <= sums and (sums-j) != x:
+                a[j-1] = 1
+                sums -= j
+    return(a)
+for t in range(int(input())):
+    x,n = [int(i) for i in input().split()]
+    sums = (n*(n+1)//2)-x
+    if n <= 3:
+        print("impossible")
+    elif sums % 2 != 0:
+        print("impossible")
+    else:
+        a = partition_it(n,sums//2,x)
+        st="".join(str(e) for e in a)
+        print(st)
