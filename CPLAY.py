d3587b088a2b6140145f47102deaf7de8a45d920
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-09-19 18:51:14 +0530

    Solution to Codechef problem CPLAY
    Link to the problem - https://www.codechef.com/DEC17/problems/CPLAY

---
+++ CPLAY.py
@@ -0,0 +1,55 @@
+try:
+    while True:
+        inp=input()
+        if inp!="":
+            a=[]
+            b=[]
+            a1=0
+            b1=0
+            c=0
+            for i in range(20):
+                if i%2==0:
+                    a.append(inp[i])
+                else:
+                    b.append(inp[i])
+            for i in range(5):
+                if a[i]=='1':
+                    a1+=1
+                if a1>b1+5-i:
+                    print("TEAM-A ",((i+1)*2)-1)
+                    c=1
+                    break
+                if b1>a1+4-i:
+                    print("TEAM-B ",((i+1)*2)-1)
+                    c=1
+                    break      
+                if b[i]=='1':
+                    b1+=1
+                if a1>b1+4-i:
+                    print("TEAM-A ",(i+1)*2)
+                    c=1
+                    break
+                if b1>a1+4-i:
+                    print("TEAM-B ",(i+1)*2)
+                    c=1
+                    break            
+            if c==0:
+                for i in range(5,10):
+                    if a[i]=='1':
+                        a1+=1               
+                    if b[i]=='1':
+                        b1+=1
+                    if a1>b1:
+                        print("TEAM-A ",(i+1)*2)
+                        c=1
+                        break
+                    if b1>a1:
+                        print("TEAM-B ",(i+1)*2)
+                        c=1
+                        break
+                if c==0:
+                    print("TIE")
+        else:
+            break
+except EOFError:
+	pass
