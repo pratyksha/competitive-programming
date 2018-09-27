b0748c99be367d317509b2f81e98321c18967c5e
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-09-27 19:45:22 +0530

    Solution to Codechef problem CARPTUN
    Link to the problem - https://www.codechef.com/FEB18/problems/CARPTUN

---
+++ CARPTUN.py
@@ -0,0 +1,5 @@
+for t in range(int(input())):
+        n=int(input())
+        a=[int(i) for i in input().split()]
+        c,d,s=[int(i) for i in input().split()]
+        print(float(max(a)*(c-1)))
