4c9d6aac5ca722bac062c67b04a87654dab48435
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-09-28 19:12:20 +0530

    Solution to Codechef problem CHEFPTNT
    Link to the problem - https://www.codechef.com/FEB18/problems/CHEFPTNT

---
+++ CHEFPTNT.py
@@ -0,0 +1,34 @@
+for t in range(int(input())):
+    n,m,x,k=[int(i) for i in input().split()]
+    s=input()
+    e=0
+    o=0
+    for i in s:
+        if i is 'E':
+            e+=1
+        elif i is 'O':
+            o+=1
+    if n>k or x==0:
+        print('no')
+    else:
+        for m1 in range(1,m+1):
+            if m1%2==0:
+                if x<e:
+                    n-=x
+                    e-=x
+                else:
+                    n-=e
+                    e=0
+            else:
+                if x<o:
+                    n-=x
+                    o-=x
+                else:
+                    n-=o
+                    o=0
+            if n<=0:
+                break
+        if n<=0:            
+            print('yes')
+        else:
+            print('no')
