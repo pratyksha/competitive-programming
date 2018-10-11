38eea23579a70ffd2f0bc172c2e870b902e51a90
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-10-11 17:45:29 +0530

    Solution to Codechef problem RD19
    Link to the problem - https://www.codechef.com/MAY18B/problems/RD19

---
+++ RD19.py
@@ -0,0 +1,13 @@
+from functools import reduce
+def hcf(num1, num2):
+    if num1 == 0:
+        return num2
+    return hcf(num2%num1, num1)
+for t in range(int(input())):
+    n = int(input())
+    a = [int(i) for i in input().split()]
+    hcf_all = reduce(hcf, a)
+    if hcf_all != 1:
+        print (-1)
+    else:
+        print (0)
