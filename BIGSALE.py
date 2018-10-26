5a062e09a44a20e0f741d73e163b16fb44541dd2
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-10-26 19:39:02 +0530

    Update BIGSALE.py

--- BIGSALE.py
+++ BIGSALE.py
@@ -3,10 +3,10 @@ def FinalPrice(p,q,d):
     p2=p1-(p1*d/100)
     p=p-p2
     return(q*p)
-for t in range(int(input())):
-    n=int(input())
+for t in range(int(input())): # number of testcases
+    n=int(input()) # number of recipe types
     sums=0
     for i in range(n):
-        p,q,d=input().split()
+        p,q,d=input().split() # price, quantity, discount
         sums+=FinalPrice(int(p),int(q),int(d))
-    print(sums)
+    print(sums) # total amount of money lost
