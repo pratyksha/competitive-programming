97b4409bf09457127669dec00eaea7e74c80188b
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-10-28 16:46:41 +0530

    Update CAPPLE.py

--- CAPPLE.py
+++ CAPPLE.py
@@ -1,8 +1,8 @@
-for t in range(int(input())):
-    n = int(input())
-    apple_per_tree = [int(i) for i in input().split()]
-    sett = set(apple_per_tree)
+for t in range(int(input())):  # number of test cases
+    n = int(input()) # number of apple trees in Chef's garden
+    apple_per_tree = [int(i) for i in input().split()] # number of apples on each tree
+    sett = set(apple_per_tree) # for trees with same number of apples, all apples are plucked in a single minute
     time = 0
     for i in sett:
         time += 1
-    print(time)
+    print(time) # minimum time to pluck all apples
