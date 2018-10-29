eb83c53a945d2cd8d9db582c40ef8a7f8b8559be
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-10-29 20:16:07 +0530

    Update CARPTUN.py

--- CARPTUN.py
+++ CARPTUN.py
@@ -1,5 +1,5 @@
-for t in range(int(input())):
-        n=int(input())
-        a=[int(i) for i in input().split()]
-        c,d,s=[int(i) for i in input().split()]
-        print(float(max(a)*(c-1)))
+for t in range(int(input())): # number of test cases
+        n=int(input()) # number of tunnels
+        a=[int(i) for i in input().split()] # time to process every i-th car at the toll booth in the tunnels
+        c,d,s=[int(i) for i in input().split()] # number of cars, distance between consecutive tunnels, velocity of each car
+        print(float(max(a)*(c-1))) # time delay between first and last car
