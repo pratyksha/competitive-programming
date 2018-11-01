ff6ca11b39088f4a15bcb7066728286b6c5a0886
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-02 02:27:23 +0530

    Update CHEFAPAR.java

--- CHEFAPAR.java
+++ CHEFAPAR.java
@@ -5,21 +5,21 @@ class Codechef
     public static void main (String[] args) throws IOException
     {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int t=Integer.parseInt(br.readLine());
+        int t=Integer.parseInt(br.readLine()); // t = number of test cases
         while(t-- >0)
         {
-        int n=Integer.parseInt(br.readLine());
-        String s=(br.readLine()).replace(" " , "");
+        int n=Integer.parseInt(br.readLine()); // n = number of months
+        String s=(br.readLine()).replace(" " , ""); // chef paid 1000rs maintenance fees or not
         int a=0,ct=0;char ch;
         for(int i=0;i<s.length();i++)
         {
             ch=s.charAt(i);
-            if(ch=='0')
-                a++; 
-            else if(ch=='1' && a>0)
-            ct++;
+            if(ch=='0') // fees not paid
+                a++; // counter for fees not paid
+            else if(ch=='1' && a>0) // fees paid
+                ct++; // counter for fees paid
         }
-        System.out.println(ct*100+a*1000+a*100);
+        System.out.println(ct*100+a*1000+a*100); // total amount that has to be paid including fine
         }
     }
 }
