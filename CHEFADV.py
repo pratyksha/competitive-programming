fe1ebdc752852af7aae1ae02a75ec0cdd185fc7b
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-01 10:58:04 +0530

    Update CHEFADV.py

--- CHEFADV.py
+++ CHEFADV.py
@@ -1,8 +1,8 @@
-for t in range(int(input())):
-    n, m, x, y = [int(i) for i in input().split()]
+for t in range(int(input())): # number of testcases
+    n, m, x, y = [int(i) for i in input().split()] # knowledge, power, increase in knowledge, increase in power
     if ((n-1) % x) == 0  and ((m-1) % y ) == 0 and n-1 >= 0 and m-1 >= 0:
-        print('Chefirnemo')
+        print('Chefirnemo') # it is possible to reach the required knowledge and power
     elif ((n-2) % x) == 0 and ((m-2) % y) == 0 and n-2 >= 0 and m-2 >= 0:
-        print('Chefirnemo')
+        print('Chefirnemo') # it is possible to reach the required knowledge and power
     else:
-        print('Pofik')
+        print('Pofik') # it is not possible to reach the required knowledge and power
