fcd9ca839f546a72292e9acf3189f052022282b2
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-03 19:05:38 +0530

    Update CHEFCHR.py

--- CHEFCHR.py
+++ CHEFCHR.py
@@ -1,11 +1,12 @@
-for t in range(int(input())):
-    s=input()
+for t in range(int(input())): # number of test cases
+    s=input() # preprocessed sentence
     mins=0
-    st=['chef', 'hcef', 'hecf', 'hefc', 'cehf', 'echf', 'ehcf', 'ehfc', 'cefh', 'ecfh', 'efch', 'efhc', 'chfe', 'hcfe', 'hfce', 'hfec', 'cfhe', 'fche', 'fhce', 'fhec', 'cfeh', 'fceh', 'fech', 'fehc']
+    # all possible combinations of chef
+    st=['chef', 'hcef', 'hecf', 'hefc', 'cehf', 'echf', 'ehcf', 'ehfc', 'cefh', 'ecfh', 'efch', 'efhc', 'chfe', 'hcfe', 'hfce', 'hfec', 'cfhe', 'fche', 'fhce', 'fhec', 'cfeh', 'fceh', 'fech', 'fehc'] 
     for word in st:
-        mins+=s.count(word)
+        mins+=s.count(word) 
     if mins==0:
         print("normal")
     else:
         print("lovely",end=" ")
-        print(mins)
+        print(mins) # number of ways to select four contiguous characters
