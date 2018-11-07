c9297d4ae814d958872a02c51045941a72781369
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-07 19:01:13 +0530

    Update CHEFMOVR.py

--- CHEFMOVR.py
+++ CHEFMOVR.py
@@ -1,6 +1,6 @@
-for t in range(int(input())):
-    n, d = [int(i) for i in input().split()]
-    a = [int(i) for i in input().split()]
+for t in range(int(input())): # number of test cases
+    n, d = [int(i) for i in input().split()] # number of elements in array and the size of mover
+    a = [int(i) for i in input().split()] # input array 
     if sum(a)%n != 0:
         print(-1)
     else:
@@ -16,6 +16,6 @@ for t in range(int(input())):
                 ct += (a[i] - p)
                 a[i] = p
         if max(a) == min(a):
-            print(ct)
+            print(ct) # minimum number of uses
         else:
-            print(-1)
+            print(-1) # its impossible to do what Snuffles wants
