af41e8e38457aa823212eb151e18695a33345a94
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-12 06:13:06 +0530

    Update CHEFSUM.C

--- CHEFSUM.C
+++ CHEFSUM.C
@@ -2,11 +2,11 @@
 int a[100000];
 int main(void) {
 	int T=0,N=0,i=0;
-	scanf("%d",&T);
+	scanf("%d",&T); //number of test cases
 	for(i=0;i<T;i++)
 	{ 
 	  int j=1,minInd=1,p=0;   
-	    scanf("%d",&N);
+	    scanf("%d",&N); // number of elements in array A
 	    for(j=1;j<=N;j++)
 	    {
 	      scanf("%d",&a[j]);
@@ -20,7 +20,7 @@ int main(void) {
 	        minInd=j;
 	        }
 	    }
-	printf("%d\n",minInd);
+	printf("%d\n",minInd); // minimized sum of prefix sum and suffix sum
 	}
 	return 0;
 }
