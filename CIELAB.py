786f0f5619cc888c81523b1bf291ffd1fe402460
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-16 00:23:00 +0530

    Update CIELAB.py

--- CIELAB.py
+++ CIELAB.py
@@ -1,7 +1,7 @@
-a,b = [int(i) for i in input().split()]
-st = a-b
-if st % 10 == 9:
+a,b = [int(i) for i in input().split()] # input
+st = a-b # difference of a and b
+if st % 10 == 9: # checking is the result's unit digit is 9
    #print(st)
-    print(st-1)
+    print(st-1) # then print (result - 1)
 else:
-    print(st+1)
+    print(st+1) # else print (result + 1)
