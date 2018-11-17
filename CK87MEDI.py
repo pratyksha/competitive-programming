df14584a86781139f5404cbc3c35d1fd9189d706
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-17 20:11:57 +0530

    Update CK87MEDI.py

--- CK87MEDI.py
+++ CK87MEDI.py
@@ -1,9 +1,9 @@
-for i in range(int(input())):
-    n,k=input().split()
+for i in range(int(input())): # number of test cases
+    n,k=input().split() # array length and numbers of elements to be inserted
     b=[]
-    a = [int(j) for j in input().split()]
+    a = [int(j) for j in input().split()] # array elements
     b=sorted(a)
     if (int(k)+int(n))//2>int(n):
         print(1000)
     else:
-        print(b[((int(k)+int(n))//2)]) 
+        print(b[((int(k)+int(n))//2)]) # greatest median after inserting k elements
