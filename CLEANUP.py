ba2fe54f2e46d9e4140276a93cb1fccdf142a0bb
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-18 11:00:33 +0530

    Update CLEANUP.py

--- CLEANUP.py
+++ CLEANUP.py
@@ -1,10 +1,10 @@
-for t in range(int(input())):
-    n, m = [ int(i) for i in input().split() ]
-    jobDone = [ int(i) for i in input().split() ]
-    jobLeft = [ int(i) for i in range(1,n+1) if i not in jobDone ]
+for t in range(int(input())): # number of test cases
+    n, m = [ int(i) for i in input().split() ] # number of jobs to do, number of jobs done
+    jobDone = [ int(i) for i in input().split() ] # jobs done
+    jobLeft = [ int(i) for i in range(1,n+1) if i not in jobDone ] # jobs to do
     for i in jobLeft[0::2]:
-        print(i,end = " ")
+        print(i,end = " ") # to be done by chef
     print()
     for i in jobLeft[1::2]:
-        print(i,end = " ")
+        print(i,end = " ") # to be done by chef's assistant
     print()
