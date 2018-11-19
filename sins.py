65182739b3449212e959f41dfbaf93b14e438e03
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-19 10:09:31 +0530

    Update sins.py

--- sins.py
+++ sins.py
@@ -2,11 +2,11 @@ def hcf(m,b):
     if (m == 0):
         return b
     return hcf(b % m, m)
-for t in range(int(input())):
-    x, y = [ int(i) for i in input().split()]
+for t in range(int(input())): # number of test cases
+    x, y = [ int(i) for i in input().split()]  # x = Meliodas's chocolates and y = Ban's chocolates
     if x == 0:
         print(y)
     elif y == 0:
         print(x)
     else:
-        print(2*hcf(x, y))
+        print(2*hcf(x, y)) # number of chocolates left after fight
