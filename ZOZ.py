596ca55957755ca2bdb2670d9c3dab11c17347b8
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-22 09:11:54 +0530

    Update ZOZ.py

--- ZOZ.py
+++ ZOZ.py
@@ -1,9 +1,9 @@
-for t in range(int(input())):
-    n, k = [int(i) for i in input().split()]
-    a = [int(i) for i in input().split()]
+for t in range(int(input())): # number of test cases
+    n, k = [int(i) for i in input().split()] # input value of n,k
+    a = [int(i) for i in input().split()] # array elements
     count = 0
     suma = sum(a)
     for i in a:
         if (i + k) > (suma - i):
             count += 1
-    print(count)
+    print(count) # number of valid positions
