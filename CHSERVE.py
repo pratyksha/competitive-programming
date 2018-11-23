7a4b0e5cfdbccd0cc3d4409d2834f3c3033ee99c
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-23 18:05:45 +0530

    Update CHSERVE.py

--- CHSERVE.py
+++ CHSERVE.py
@@ -1,7 +1,7 @@
-for _ in range(int(input())):
-    chef, cook, k = [int(i) for i in input().split()]
+for _ in range(int(input())): # number of test cases
+    chef, cook, k = [int(i) for i in input().split()] # number of points scored by Chef, Cook, value of k
     if (chef+cook) % (2*k) < k:
-        print("CHEF")
+        print("CHEF") # Chef's turn
     else:
-        print("COOK")
+        print("COOK") # Cook's turn
     
