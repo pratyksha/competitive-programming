7734a402b82d0f6615928aff5871534617bb0673
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-24 09:49:14 +0530

    Update HS08TEST.C

--- HS08TEST.C
+++ HS08TEST.C
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void) {
-    int i=0;
+    int i=0; 
     float o=0;
-    scanf("%d %f",&i,&o);
+    scanf("%d %f",&i,&o); // i = amt of cash to withdraw, o = initial account balance
     if(i%5!=0||(i+0.50)>o)
-    printf("%.2f",o);
+    printf("%.2f",o); // since balance not enough, print current balance
     else
-    printf("%.2f",o-(i+0.50));
+    printf("%.2f",o-(i+0.50)); // print account balance after the attempted transaction
 	return 0;
 }
