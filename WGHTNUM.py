3ec2b2917b3186e8a301d5bc1edb7ee34feaffd6
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-26 18:04:14 +0530

    Update WGHTNUM.py

--- WGHTNUM.py
+++ WGHTNUM.py
@@ -1,8 +1,8 @@
-for t in range(int(input())):
-    n, w = [int(i) for i in input().split()]
+for t in range(int(input())): # number of test cases
+    n, w = [int(i) for i in input().split()] #  n = number of digits and w = the required weight.
     ct = 0
     for i in range(1,10):
         for j in range(10):
             if j-i == w:
                 ct += 1
-    print((ct*pow(10, n-2, 10**9+7))%(10**9+7))
+    print((ct*pow(10, n-2, 10**9+7))%(10**9+7)) # n digit positive integer with weight w
