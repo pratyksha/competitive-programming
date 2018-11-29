2efebd081b9461abbd57f60091fbb2e4de0fe22c
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-29 09:39:07 +0530

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,6 +1,6 @@
 while (True):
-    i=int(input())
-    if i==42:
+    i=int(input()) # accept the number
+    if i==42: # if 42 is encountered, break
         break
     else:
-        print(i)
+        print(i) # print the number
