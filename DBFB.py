c0e3364a1369ed044a706370035acbac38615f72
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-11-30 08:55:24 +0530

    Update DBFB.py

--- DBFB.py
+++ DBFB.py
@@ -1,7 +1,7 @@
-for t in range(int(input())):
-    m, n = [ int(i) for i in input().split()]
-    a = [ int(i) for i in input().split()]
-    b = [ int(i) for i in input().split()]
+for t in range(int(input())): # number of test cases
+    m, n = [ int(i) for i in input().split()] # m = size of array a, n = size of array b
+    a = [ int(i) for i in input().split()] # accept array a
+    b = [ int(i) for i in input().split()] # accept array b
     res = []
     if n == 1:
         print((m*sum(a)) % 1000000007)
@@ -12,4 +12,4 @@ for t in range(int(input())):
         res.append(m*sum(b))
         for i in range(1,n):
             res.append(res[i-1]+res[i])
-        print(res[n-1] % 1000000007) 
+        print(res[n-1] % 1000000007) # print result
