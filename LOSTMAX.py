e34cdb13f1569d0be1c443ddd75318f6f898a5ca
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-12-02 13:19:37 +0530

    Update LOSTMAX.py

--- LOSTMAX.py
+++ LOSTMAX.py
@@ -1,8 +1,8 @@
-for t in range(int(input())):
-    arr = [int(i) for i in input().split()]
+for t in range(int(input())): # number of test cases
+    arr = [int(i) for i in input().split()] # n numbers
     maxa = max(arr)
     if (len(arr) - 1) != maxa:
-        print(maxa)
+        print(maxa) # max value of the n numbers
     else:
         arr.remove(len(arr)-1)
-        print(max(arr))
+        print(max(arr)) # max value of the n numbers
