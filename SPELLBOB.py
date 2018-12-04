874b483550ad4dd8ef68a8f8e051e88e430798d7
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-12-04 18:02:08 +0530

    Update SPELLBOB.py

--- SPELLBOB.py
+++ SPELLBOB.py
@@ -1,6 +1,6 @@
-for t in range(int(raw_input())):
-    card_face = raw_input()
-    card_back = raw_input()
+for t in range(int(raw_input())): # number of test cases
+    card_face = raw_input() #  characters on top faces of the first, second and third card
+    card_back = raw_input() #  characters on bottom faces of the first, second and third card
     if card_face[0] == 'b' or card_back[0] == 'b':
         if card_face[1] == 'o' or card_back[1] == 'o':
             if card_face[2] == 'b' or card_back[2] == 'b':
