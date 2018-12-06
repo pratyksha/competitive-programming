4eabaabcf9ab46a80060f941c643ff964f47f50a
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-12-06 18:54:34 +0530

    Update RECTANGL.py

--- RECTANGL.py
+++ RECTANGL.py
@@ -1,10 +1,10 @@
-for t in range(int(input())):
-    st=[int(x) for x in input().split()]
+for t in range(int(input())): # number of test cases
+    st=[int(x) for x in input().split()] # the dimensions
     c=0
     for i in st:
         if st.count(i)>=2 and st.count(i)!=3:
             c+=1
     if c%2==0 and c!=0 and c>2:
-        print("YES")
+        print("YES") # yes, it is a rectangle
     else:
-        print("NO")
+        print("NO") # no, it is not a rectangle
