f77fd8ffeade0c59e57653fcd4e0b444de118b45
pratyksha <32895655+pratyksha@users.noreply.github.com>
2018-12-27 00:18:09 +0530

    Update RD19.py

--- RD19.py
+++ RD19.py
@@ -3,8 +3,8 @@ def hcf(num1, num2):
     if num1 == 0:
         return num2
     return hcf(num2%num1, num1)
-for t in range(int(input())):
-    n = int(input())
+for t in range(int(input())): # number of test cases 
+    n = int(input()) # size of array a
     a = [int(i) for i in input().split()]
     hcf_all = reduce(hcf, a)
     if hcf_all != 1:
