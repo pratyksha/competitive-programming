8a2372ddf8a7a8fae35eac0ecedad06ecd3d36f9
pratyksha <32895655+pratyksha@users.noreply.github.com>
2019-01-08 12:41:31 +0530

    Update LISDIGIT.py

--- LISDIGIT.py
+++ LISDIGIT.py
@@ -1,6 +1,7 @@
-for t in range(int(input())):
+for t in range(int(input())): # number of test cases
     n=input()   
     a=[str(x) for x in input().split()]
     for i in a:
-        print(i, end="")
+        print(i, end="") # print the answer
+        
     print ("\n")
