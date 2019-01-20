d4593bb19467eb6a0eca99ebcbc344ade4e46ba7
pratyksha <32895655+pratyksha@users.noreply.github.com>
2019-01-20 12:22:34 +0530

    Update UNICOURS.py

--- UNICOURS.py
+++ UNICOURS.py
@@ -1,5 +1,5 @@
-for t in range(int(input())):
+for t in range(int(input())): # number of test cases 
         n=int(input())
         a=[int(i) for i in input().split()]
         p=max(a)
-        print(n-p)
+        print(n-p) # print the answer 
