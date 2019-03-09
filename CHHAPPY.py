b1bcdae12a3e4bfc0cb9bf0aca97fbdd15c095e5
pratyksha <32895655+pratyksha@users.noreply.github.com>
2019-03-09 22:56:57 +0530

    Update CHHAPPY.py

--- CHHAPPY.py
+++ CHHAPPY.py
@@ -1,13 +1,13 @@
-for _ in range(int(raw_input())):
+for _ in range(int(raw_input())):# Running for the given number of test cases
     n = int(raw_input())
-    a = [int(i) for i in raw_input().split()]
+    a = [int(i) for i in raw_input().split()]# Accepting input
     b = list(set(a))
     c = []
     ct = 0
     for i in range(len(b)):
-        c.append(a[b[i]-1])
+        c.append(a[b[i]-1])#Creating the list c
     d = set(c)
     if len(c) != len(d):
-        print("Truly Happy")
+        print("Truly Happy")#If necessary condition is met
     else:
-        print("Poor Chef")
+        print("Poor Chef")#If condition is not met
