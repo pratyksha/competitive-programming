d08856fda43155c640b3982da5687b702893fd4a
pratyksha <32895655+pratyksha@users.noreply.github.com>
2019-03-27 22:18:39 +0530

    Update CHFAR.py

--- CHFAR.py
+++ CHFAR.py
@@ -1,8 +1,8 @@
-for _ in range(int(input())):
+for _ in range(int(input())): # number of test cases
     n, k = [ int(i) for i in input().split()]
     a = [ int(i) for i in input().split()]
     count_1 = a.count(1)
     if (n - count_1) <= k:
-        print("YES")
+        print("YES") # it is possible to satisfy given condition
     else:
-        print("NO")
+        print("NO") # it is not possible to satisfy given codition
